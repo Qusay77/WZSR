7990aa4e92a585ee3f8f30d93521ba554fa678d5
Qusay <qusay@nadsoft.net>
2023-05-18 17:12:26 +0300

    query params

--- src/lib.tsx
+++ src/lib.tsx
@@ -1,31 +1,67 @@
+/* eslint-disable no-console */
 import { Global, ThemeProvider } from "@emotion/react";
 import { store } from "store";
 import { Provider } from "react-redux";
 import { GlobalStyles, theme } from "./Globals/global";
 import SRWindow from "packages/Screens/SRWindow";
+import { useEffect, useState } from "react";
+type ParamsType = {
+	sessionId?: string;
+	orgId?: string;
+	sessionDate?: string;
+};
+function paramsToObject(
+	entries: IterableIterator<[string, string]>,
+): ParamsType {
+	const result: { [key: string]: string } = {};
+	let entry = entries.next();
+	while (!entry.done) {
+		const [key, value] = entry.value;
+		result[key] = value;
+		entry = entries.next();
+	}
+	return result;
+}
 
 const SessionRecording = ({
 	container,
 }: {
 	container: { attributes: NamedNodeMap };
 }) => {
-	// eslint-disable-next-line no-console
+	const [queryParams, setQueryParams] = useState<ParamsType>({
+		sessionId: undefined,
+		orgId: undefined,
+		sessionDate: undefined,
+	});
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const entries = urlParams.entries();
+		const params = paramsToObject(entries);
+		setQueryParams(params);
+	}, []);
+	const { sessionId, orgId, sessionDate } = queryParams;
+
 	const sessionIdValue = container.attributes.getNamedItem("sessionId")?.value;
 	const orgIdValue = container.attributes.getNamedItem("orgId")?.value;
 	const sessionDateValue =
 		container.attributes.getNamedItem("sessionDate")?.value;
-	const sessionId = sessionIdValue ? parseInt(sessionIdValue, 10) : undefined;
-	const orgId = orgIdValue ? parseInt(orgIdValue, 10) : undefined;
-	// eslint-disable-next-line no-console
-	console.log(sessionIdValue, orgIdValue, sessionDateValue);
+	const sessionIdProps = sessionIdValue
+		? parseInt(sessionIdValue, 10)
+		: undefined;
+	const orgIdProp = orgIdValue ? parseInt(orgIdValue, 10) : undefined;
+	console.log(sessionId, orgId, sessionDate, "params");
+	console.log(sessionIdProps, orgIdValue, sessionDateValue, "props");
+
+	const parsedSessionId = sessionId ? parseInt(sessionId, 10) : undefined;
+	const parsedOrgId = orgId ? parseInt(orgId, 10) : undefined;
 	return (
 		<Provider store={store}>
 			<Global styles={GlobalStyles} />
 			<ThemeProvider theme={theme}>
 				<SRWindow
-					sessionId={sessionId}
-					orgId={orgId}
-					sessionDate={sessionDateValue}
+					sessionId={parsedSessionId ?? sessionIdProps}
+					orgId={parsedOrgId ?? orgIdProp}
+					sessionDate={sessionDate ?? sessionDateValue}
 				/>
 			</ThemeProvider>
 		</Provider>
--- src/page.tsx
+++ src/page.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 // import SessionRecording from "./lib";
 import "https://sr.webeyez.dev/module/srIIFE.js";
-
+// import "../module/srIIFE";
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement);
