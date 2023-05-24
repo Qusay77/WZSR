26f72805a827d6b0a31d24a92fe8c23153cd6569
Qusay <qusay@nadsoft.net>
2023-05-24 11:11:19 +0300

    search

--- packages/UIKit/SessionEvents/SessionTimeLine/SessionTimeLine.tsx
+++ packages/UIKit/SessionEvents/SessionTimeLine/SessionTimeLine.tsx
@@ -5,17 +5,46 @@ import { SessionTimeLineContainer } from "./Blocks";
 import TimeLineSearch from "./Blocks/SearchInputBlocks";
 import SessionTimeLineHeader from "./Blocks/SessionHeaderBlocks";
 import TimeLineBlock from "./TimeLineBlock";
+import { GroupType } from "./TimeLineBlock/types";
 const SessionTimeLine = () => {
 	const { events, searchValue } = useSelector(
 		({ EventsState }: { EventsState: EventsDetails }) => EventsState,
 	);
+
+	const SearchCriteria = [
+		"error_message",
+		"method",
+		"status",
+		"name",
+		"custom_data",
+	];
+
 	const filteredEvents = events
 		.map((f) => {
-			const { name, expandPageView } = f;
 			const FilteredEvent = cloneDeep(f);
-			const viewNames = expandPageView.data.filter(({ name }) =>
-				name?.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()),
-			);
+
+			const { name, expandPageView } = FilteredEvent;
+			const { data } = expandPageView;
+			const viewNames = data.filter((obj: GroupType) => {
+				if (obj.custom_data) {
+					const parsed = JSON.parse(obj.custom_data as string);
+					obj.custom_data = parsed
+						.map((p: { [key: string]: string }) => Object.values(p))
+						.flat()
+						.join("®");
+				}
+				const matches = Object.keys(obj).some((key) => {
+					return (
+						SearchCriteria.includes(key) &&
+						obj[key]
+							?.toString()
+							.toLowerCase()
+							.includes(searchValue.toLocaleLowerCase())
+					);
+				});
+				return matches;
+			});
+
 			FilteredEvent.expandPageView.data = viewNames;
 
 			const SurfaceNameIncluded = name
--- packages/UIKit/SessionEvents/SessionTimeLine/TimeLineBlock/types/index.ts
+++ packages/UIKit/SessionEvents/SessionTimeLine/TimeLineBlock/types/index.ts
@@ -7,8 +7,18 @@ type GroupType = {
 	duration?: string;
 	method?: string;
 	status?: number;
-	custom_data?: Array<{ definitionName: string; value: string }> | null;
+	custom_data?:
+		| Array<{ definitionName: string; value: string }>
+		| string
+		| null;
 	error_message: string;
+	[key: string]:
+		| string
+		| undefined
+		| number
+		| null
+		| boolean
+		| Array<{ definitionName: string; value: string }>;
 };
 
 export type { GroupType };
